90f4ce4e4dc4e27fb1878d62f743eadcf0e1a0ca
UtsavH <Utsavhudka2002@gmail.com>
2025-03-04 09:26:42 -0400

    fix(hero): dynamically fetch country names for search dropdown

--- src/app/api/data.tsx
+++ src/app/api/data.tsx
@@ -1,88 +1,220 @@
 
-export const Countries = [
+export const Countriess = [
     {
-        flag: "/images/flag/france.svg",
-        name: "France",
-        offfers: "1500 ",
-        class: "lg:lg:mt-0 mt-16",
-        delay: "0"
-    },
-    {
-        flag: "/images/flag/spain.svg",
-        name: "Spain",
-        offfers: "135",
-        class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
-        delay: "100"
-    },
-    {
-        flag: "/images/flag/china.svg",
-        name: "China",
-        offfers: "1593",
-        class: "lg:mt-0 mt-16",
-        delay: "200"
-    },
-    {
-        flag: "/images/flag/turkey.svg",
-        name: "Turkey",
-        offfers: "69",
-        class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
-        delay: "300"
-    },
-    {
-        flag: "/images/flag/germany.svg",
-        name: "Germany",
-        offfers: "589",
-        class: "lg:mt-0 mt-16",
-        delay: "400"
-    },
-    {
-        flag: "/images/flag/thailand.svg",
-        name: "Thailand",
-        offfers: "3600",
-        class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
-        delay: "500"
-    },
-    {
-        flag: "/images/flag/mexico.svg",
-        name: "Mexico",
-        offfers: "330",
-        class: "lg:mt-0 mt-16",
-        delay: "600"
-    },
-    {
-        flag: "/images/flag/canada.svg",
-        name: "Canada",
-        offfers: "1030",
-        class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
-        delay: "700"
-    },
-    {
-        flag: "/images/flag/brazil.svg",
-        name: "Brazil",
-        offfers: "890",
-        class: "lg:mt-0 mt-16",
-        delay: "800"
-    },
-    {
-        flag: "/images/flag/india.svg",
-        name: "India",
-        offfers: "3100",
-        class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
-        delay: "900"
-    },
-    {
-        flag: "/images/flag/colombia.svg",
-        name: "Colombia",
-        offfers: "150",
-        class: "lg:mt-0 mt-16",
-        delay: "1000"
-    },
-    {
-        flag: "/images/flag/malaysia.svg",
-        name: "Malaysia",
-        offfers: "1500",
-        class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
-        delay: "1100"
-    },
+        "countries": [
+          {
+            "name": "Japan",
+            "language": "Japanese",
+            "traditional_dress": "Kimono",
+            "traditional_food": "Sushi, Ramen, Tempura",
+            "traditional_activities": "Tea Ceremony, Sumo Wrestling, Hanami (Cherry Blossom Viewing)",
+            "national_anthem": "Kimigayo",
+            "famous_places": [
+              {
+                "name": "Mount Fuji",
+                "review": "A breathtaking sight and a must-visit for nature lovers."
+              },
+              {
+                "name": "Kyoto Temples",
+                "review": "Rich in history and culture, offering a serene atmosphere."
+              }
+            ]
+          },
+          {
+            "name": "France",
+            "language": "French",
+            "traditional_dress": "Breton Costume, Alsatian Dress",
+            "traditional_food": "Baguette, Croissant, Coq au Vin",
+            "traditional_activities": "Wine Tasting, Bastille Day Celebrations, Tour de France",
+            "national_anthem": "La Marseillaise",
+            "famous_places": [
+              {
+                "name": "Eiffel Tower",
+                "review": "Iconic landmark with stunning views of Paris."
+              },
+              {
+                "name": "Louvre Museum",
+                "review": "Home to the Mona Lisa and a vast collection of art."
+              }
+            ]
+          },
+          {
+            "name": "India",
+            "language": "Hindi, English",
+            "traditional_dress": "Saree, Kurta-Pajama",
+            "traditional_food": "Biryani, Dosa, Butter Chicken",
+            "traditional_activities": "Yoga, Festivals like Diwali & Holi, Classical Dance",
+            "national_anthem": "Jana Gana Mana",
+            "famous_places": [
+              {
+                "name": "Taj Mahal",
+                "review": "A symbol of love and architectural brilliance."
+              },
+              {
+                "name": "Jaipur’s Palaces",
+                "review": "A royal experience with stunning historical sites."
+              }
+            ]
+          },
+          {
+            "name": "Canada",
+            "language": "English, French",
+            "traditional_dress": "Inuit Parka, Scottish Kilts",
+            "traditional_food": "Poutine, Butter Tarts, Maple Syrup",
+            "traditional_activities": "Ice Hockey, Canoeing, Indigenous Pow Wows",
+            "national_anthem": "O Canada",
+            "famous_places": [
+              {
+                "name": "Niagara Falls",
+                "review": "One of the most stunning natural wonders in the world."
+              },
+              {
+                "name": "Banff National Park",
+                "review": "A paradise for nature lovers with breathtaking mountain views."
+              }
+            ]
+          },
+          {
+            "name": "Vietnam",
+            "language": "Vietnamese",
+            "traditional_dress": "Áo dài",
+            "traditional_food": "Pho, Banh Mi, Goi Cuon",
+            "traditional_activities": "Water Puppetry, Tet Festival, Street Markets",
+            "national_anthem": "Tiến Quân Ca",
+            "famous_places": [
+              {
+                "name": "Ha Long Bay",
+                "review": "A mesmerizing seascape with stunning limestone islands."
+              },
+              {
+                "name": "Hoi An Ancient Town",
+                "review": "A charming town full of history and vibrant lanterns."
+              }
+            ]
+          },
+          {
+            "name": "Italy",
+            "language": "Italian",
+            "traditional_dress": "Trentino Folk Costume",
+            "traditional_food": "Pizza, Pasta, Risotto",
+            "traditional_activities": "Opera, Carnivals, Vespa Riding",
+            "national_anthem": "Il Canto degli Italiani",
+            "famous_places": [
+              {
+                "name": "Colosseum",
+                "review": "An ancient amphitheater full of history and grandeur."
+              },
+              {
+                "name": "Venice Canals",
+                "review": "A romantic and unique way to explore the city."
+              }
+            ]
+          },
+          {
+            "name": "Brazil",
+            "language": "Portuguese",
+            "traditional_dress": "Baiana Dress, Gaucho Outfit",
+            "traditional_food": "Feijoada, Pão de Queijo, Churrasco",
+            "traditional_activities": "Samba Dancing, Capoeira, Carnival Celebrations",
+            "national_anthem": "Hino Nacional Brasileiro",
+            "famous_places": [
+              {
+                "name": "Christ the Redeemer",
+                "review": "One of the new wonders of the world with incredible views."
+              },
+              {
+                "name": "Amazon Rainforest",
+                "review": "A must-visit for nature lovers and adventure seekers."
+              }
+            ]
+          }
+        ]
+      }      
 ];
 
+export const Countries = [
+  {
+      flag: "/images/flag/france.svg",
+      name: "France",
+      offfers: "1500 ",
+      class: "lg:lg:mt-0 mt-16",
+      delay: "0"
+  },
+  {
+      flag: "/images/flag/spain.svg",
+      name: "Spain",
+      offfers: "135",
+      class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
+      delay: "100"
+  },
+  {
+      flag: "/images/flag/china.svg",
+      name: "China",
+      offfers: "1593",
+      class: "lg:mt-0 mt-16",
+      delay: "200"
+  },
+  {
+      flag: "/images/flag/turkey.svg",
+      name: "Turkey",
+      offfers: "69",
+      class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
+      delay: "300"
+  },
+  {
+      flag: "/images/flag/germany.svg",
+      name: "Germany",
+      offfers: "589",
+      class: "lg:mt-0 mt-16",
+      delay: "400"
+  },
+  {
+      flag: "/images/flag/thailand.svg",
+      name: "Thailand",
+      offfers: "3600",
+      class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
+      delay: "500"
+  },
+  {
+      flag: "/images/flag/mexico.svg",
+      name: "Mexico",
+      offfers: "330",
+      class: "lg:mt-0 mt-16",
+      delay: "600"
+  },
+  {
+      flag: "/images/flag/canada.svg",
+      name: "Canada",
+      offfers: "1030",
+      class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
+      delay: "700"
+  },
+  {
+      flag: "/images/flag/brazil.svg",
+      name: "Brazil",
+      offfers: "890",
+      class: "lg:mt-0 mt-16",
+      delay: "800"
+  },
+  {
+      flag: "/images/flag/india.svg",
+      name: "India",
+      offfers: "3100",
+      class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
+      delay: "900"
+  },
+  {
+      flag: "/images/flag/colombia.svg",
+      name: "Colombia",
+      offfers: "150",
+      class: "lg:mt-0 mt-16",
+      delay: "1000"
+  },
+  {
+      flag: "/images/flag/malaysia.svg",
+      name: "Malaysia",
+      offfers: "1500",
+      class: "mt-16 before:content-[''] before:absolute before:w-px before:h-8 before:bg-primary before:-left-px before:bottom-4",
+      delay: "1100"
+  },
+];
