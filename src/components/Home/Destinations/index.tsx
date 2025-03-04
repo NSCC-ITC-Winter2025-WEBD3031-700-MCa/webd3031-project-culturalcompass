771fe7919bb89bac5bb4ab7365c8695ccd68153a
UtsavH <Utsavhudka2002@gmail.com>
2025-03-04 11:39:24 -0400

    feat(ui): enhance 'Top Destination' section with improved UX

--- src/components/Home/Destinations/index.tsx
+++ src/components/Home/Destinations/index.tsx
@@ -15,7 +15,7 @@ const Destination = () => {
             data-aos-duration="1000"
           >
             <Image
-              src="/images/destination/destinations-1.jpg"
+              src="/images/destination/Canada.jpg"
               alt="..."
               width={500}
               height={0}
@@ -24,11 +24,11 @@ const Destination = () => {
             />
             <div className="p-8 sm:ps-8 ps-4 absolute bottom-0 left-0">
               <h4 className="text-white dark:text-white sm:text-2xl text-xl mb-2">
-                Hiking Adventure
+                Majestic Peaks – Canada’s Untamed Beauty
               </h4>
-              <p className="text-white sm:text-lg text-base">
+              {/* <p className="text-white sm:text-lg text-base">
                 306 Destinations
-              </p>
+              </p> */}
             </div>
           </div>
           <div className="lg:col-span-6 col-span-12 flex flex-col gap-7">
@@ -39,7 +39,7 @@ const Destination = () => {
               data-aos-duration="1000"
             >
               <Image
-                src="/images/destination/destinations-2.jpg"
+                src="/images/destination/India.jpg"
                 alt="..."
                 width={500}
                 height={0}
@@ -48,11 +48,11 @@ const Destination = () => {
               />
               <div className="p-8 sm:ps-8 ps-4 absolute bottom-0 left-0">
                 <h4 className="text-white dark:text-white sm:text-2xl text-xl mb-2">
-                  Outdoor Adventure
+                  Timeless Marvel - A Symbol of Eternal Love 
                 </h4>
-                <p className="text-white sm:text-lg text-base">
+                {/* <p className="text-white sm:text-lg text-base">
                   418 Destinations
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="grid grid-cols-2 gap-7">
@@ -63,21 +63,19 @@ const Destination = () => {
                 data-aos-duration="1000"
               >
                 <Image
-                  src="/images/destination/destinations-3.jpg"
+                  src="/images/destination/travel.usnews.jpg"
                   alt=".."
                   width={200}
                   height={0}
-                  style={{ width: "100%", height: "auto" }}
+                  style={{ width: "100%", height: "100%" }}
                   className="group-hover:scale-110 duration-300"
                 />
-                <div className="sm:p-8 p-4 absolute bottom-0 left-0">
-                  <h4 className="text-white dark:text-white sm:text-2xl text-xl mb-2">
-                    Beach Adventure
-                  </h4>
-                  <p className="text-white sm:text-lg text-base">
-                    85 Destinations
-                  </p>
-                </div>
+                {/* <div className="sm:p-8 p-4 absolute bottom-0 left-0">
+                  <h5 className="text-white dark:text-white sm:text-2xl text-xl mb-2">
+                    Japan’s Iconic Peak of Reverence
+                  </h5>
+                  
+                </div> */}
               </div>
               <div
                 className="relative rounded-3xl overflow-hidden group"
@@ -85,20 +83,20 @@ const Destination = () => {
                 data-aos-duration="1000"
               >
                 <Image
-                  src="/images/destination/destinations-4.jpg"
+                  src="/images/destination/Vietnam.jpg"
                   alt=".."
                   width={200}
                   height={0}
-                  style={{ width: "100%", height: "auto" }}
+                  style={{ width: "100%", height: "100%" }}
                   className="group-hover:scale-110 duration-300"
                 />
                 <div className="sm:p-8 p-4 absolute bottom-0 left-0">
-                  <h4 className="text-white dark:text-white sm:text-2xl text-xl mb-2">
-                    Pilgrimage Destination
-                  </h4>
-                  <p className="text-white sm:text-lg text-base">
+                  {/* <h5 className="text-gray-300 dark:text-gray-300 sm:text-2xl text-xl mb-2">
+                    Ha Long Bay’s Mystical Charm.
+                  </h5> */}
+                  {/* <p className="text-gray-400 sm:text-lg text-base">
                     29 Destinations
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </div>
