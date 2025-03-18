790a7dd7c4f44ff424041baa5d75369f0571f678
Maris Hume <marishume@gmail.com>
2025-03-18 10:07:41 -0300

    Intial Dashboard

---
+++ src/app/(site)/dashboard/page.tsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+import HeroSub from "@/components/SharedComponent/HeroSub";
+import { Metadata } from "next";
+import Destination from "@/components/Home/Destinations";
+import Dashboard from "@/components/Home/Dashboard";
+export const metadata: Metadata = {
+    title: "Destination | Culture Compass",
+};
+
+const page = () => {
+  const breadcrumbLinks = [
+    { href: "/", text: "Home" },
+    { href: "/dashboard", text: "Dashboard" },
+  ];
+  return (
+    <>
+      <HeroSub
+        title="Dashboard"
+        description="Discover a wealth of insightful materials meticulously crafted to provide you with a comprehensive understanding of the latest trends."
+        breadcrumbLinks={breadcrumbLinks}
+      />
+      <Dashboard />
+    </>
+  );
+};
+
+export default page;
