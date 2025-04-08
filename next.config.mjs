721ccfc6f9fb4670d5d0b40fc3ee42988a27ad4e
Maris Hume <marishume@gmail.com>
2025-04-08 09:34:24 -0300

    feat: Added jest to add some basic checks to the hero component

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
-    reactStrictMode: false,
-    typescript: {
-        ignoreBuildErrors: true, // This will ignore TypeScript errors during build
-      },
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(new URL('.', import.meta.url).pathname), // Resolve '@' to the root directory of the project
+    };
+    return config;
+  },
 };
- 
+
 export default nextConfig;
