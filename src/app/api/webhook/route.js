fbcdc4d6c4a0a43fa482d31197341ff2ccc27d2e
Linh Le <lethimylinh.2001@gmail.com>
2025-04-08 14:00:23 -0300

    fix: update code to reset session after payment2

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -2,8 +2,8 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 import { PrismaClient } from '@prisma/client';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
  
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2023-10-16',
